2d597671134d0029eba420f1eb58ff28c1ce018a
Aerim Yi <aerimyi@192-168-1-104.tpgi.com.au>
2022-03-02 08:39:35 +1100

    added weather api

--- js/index.js
+++ js/index.js
@@ -148,6 +148,56 @@ $('#toggleAccordion').on('click', function () {
     }
 });
 
+// Weather API
+const getWeather = async city => {
+    try {
+        const apiKey = "92329e9f76cb0a86293be4e3fa64f5c9"
+        const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        const data = res.data
+        console.log(data)
+        addWeather(data)
+    } catch (e) {
+        console.log('Error!', e)
+    }
+}
+const addWeather = data => {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = Math.floor(temp) + "°C";
+    document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.querySelector(".weatherCard").style.backgroundImage =
+        "url('https://source.unsplash.com/255x305/?" + name + "')";
+}
+
+function search() {
+    getWeather(document.querySelector(".search-bar").value)
+}
+document.querySelector(".search button")
+    .addEventListener("click", search);
+document.querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            search();
+        }
+    });
+
+getWeather("Sydney");
+
+
+
+
+
 
 
 
