a0fc05598fde2706ced3417239c0d24dee0fed6d
Aerim Yi <aerimyi@192-168-1-104.tpgi.com.au>
2022-03-03 14:34:45 +1100

    finished profile feature

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+//Initiate, load and render the weather
+const profileManager = new ProfileManager('Stranger', 'Sydney');
+profileManager.load()
+profileManager.render()
+
 //Initiate, load and render the data
 const taskManager = new TaskManager();
 taskManager.load()
@@ -147,71 +152,6 @@ $('#toggleAccordion').on('click', function () {
         $(this).addClass("btn-info border-info")
     }
 });
-
-// start page
-const startContainer = document.querySelector('#startContainer');
-const btnStart = document.querySelector('#btnStart');
-const navbarContainer = document.querySelector('#navbarContainer')
-const taskContainer = document.querySelector('#taskContainer')
-
-btnStart.addEventListener('click', function (e) {
-    startContainer.classList.add('d-none')
-    startContainer.classList.remove('d-flex')
-    navbarContainer.classList.remove('d-none')
-    taskContainer.classList.remove('d-none')
-
-    const firstNameInput = document.querySelector('#firstNameInput').value
-    if (firstNameInput) {
-        firstName.innerText = firstNameInput
-    }
-
-    search();
-});
-
-// Weather API
-const getWeather = async city => {
-    try {
-        const apiKey = "92329e9f76cb0a86293be4e3fa64f5c9"
-        const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-        const data = res.data
-        addWeather(data)
-    } catch (e) {
-        console.log('Error!', e)
-    }
-}
-const addWeather = data => {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = Math.floor(temp) + "°C";
-    document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText =
-        "Wind speed: " + speed + " km/h";
-    document.querySelector(".weatherCard").style.backgroundImage =
-        "url('https://source.unsplash.com/255x305/?" + name + "')";
-}
-
-function search() {
-    getWeather(document.querySelector(".search-city").value)
-}
-
-
-
-getWeather("Sydney");
-
-
-
-
-
-
-
 
 
 
